1f4c1075aeced28619216ddaf13a3f20d0732713
AndreyDyban <andrey.dyban@gmail.com>
2017-01-22 09:08:49 +0200

    added logger configuration

--- src/main/java/ua/kiev/husky/mvc/MainController.java
+++ src/main/java/ua/kiev/husky/mvc/MainController.java
@@ -4,18 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ua.kiev.husky.model.Course;
 import ua.kiev.husky.service.CourseService;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+
+import java.util.List;
 
 
 @Controller
 public class MainController {
+
+
+    private static final Logger LOGGER = LogManager.getLogger(MainController.class);
 
     @Autowired
     private CourseService courseService;
 
     @RequestMapping("/")
     public String index(Model model){
-        model.addAttribute("courses", courseService.findAll());
+        final List<Course> allCourses = courseService.findAll();
+        model.addAttribute("courses", allCourses);
+        LOGGER.info("Found courses : " + allCourses);
         return "index";
     }
 
